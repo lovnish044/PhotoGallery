e71b1e7390369aa1efaec83ccaf80058b9f6a3bb
lovnish044 <lovnishgarg044@gmail.com>
2021-10-26 09:08:28 +0530

    Updates

---
+++ precache-manifest.061277faeaa0f15c29d160f16741ca3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f0822a28960fba44d27d915da77599b",
+    "url": "/Full-stack/index.html"
+  },
+  {
+    "revision": "fd858cd9d78b01a2b3c0",
+    "url": "/Full-stack/static/css/main.a1d481cb.chunk.css"
+  },
+  {
+    "revision": "20ae1808e1c9af56956c",
+    "url": "/Full-stack/static/js/2.bfb52602.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/Full-stack/static/js/2.bfb52602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd858cd9d78b01a2b3c0",
+    "url": "/Full-stack/static/js/main.64600d67.chunk.js"
+  },
+  {
+    "revision": "f7cbc7f8f91aac333745",
+    "url": "/Full-stack/static/js/runtime-main.43b55273.js"
+  }
+]);
